d51cd307c270290c81c496647b5635cd32131411
TatsuyaSekiguchi23 <22z1042k@student.gs.chiba-u.jp>
2025-07-16 23:55:13 +0900

    I can't do well

--- day05_homework.R
+++ day05_homework.R
@@ -1,12 +1,14 @@
 # DAY 5 HOMEWORK
 #
-# Name:
+# Name: Tatsuya Sekiguchi
 
 # NOTE: In the text below, backticks ``
 # are used to indicate code. For example, `library(tidyverse)`
 
 # Before doing anything else, load the packages you need for this assignment:
 # `tidyverse` and `nycflights13`
+library(tidyverse)
+library(nycflights13)
 
 # Q1. The `nycflights13` package includes a dataframe called `planes` that
 # contains data for each airplane such as manufacturer, model, etc.
@@ -16,16 +18,31 @@
 #
 # HINT: the `tailnum` column uniquely identifies each airplane.
 
+flights %>%
+  left_join(planes, join_by(tailnum))
+
+answer_1 <- flights %>%
+  left_join(planes, join_by(tailnum))
+
+
 # Q2. Use answer_1 to count the number of flights made by each manufacturer,
 # then sort the results so the manufacturer with the most flights is at the top.
 # Save the result as answer_2
 #
 # HINT: use `count()`
+  
+answer_1 %>%
+  count(manufacturer)
+
+answer_2 <- "BOEING"
 
 # Q3. Does every flight in the `flights` dataframe have a corresponding
 # airplane in `planes`?
 #
 # Save your answer as a logical vector (either TRUE or FALSE) to answer_3
+
+answer_3 <- FALSE
+#i don't have ideas
 
 # Q4. The `nycflights13` package includes a dataframe called `airlines` that
 # includes the name of each airline and its corresponding carrier code.
@@ -33,6 +50,12 @@
 #
 # Join the `airlines` dataframe to the `flights` dataframe, keeping all rows in
 # `flights`. Save the result as answer_4
+
+flights %>%
+  left_join(airlines)
+
+answer_4 <- flights %>%
+  left_join(airlines)
 
 # Q5. Use answer_4 to find the average arrival delay for each airline.
 # Exclude missing values from your calculation.
@@ -44,3 +67,6 @@
 # HINT: when you use functions like `mean()`, `max()`, or `min()`, use the
 # option `na.rm = TRUE` to exclude missing values from the calculation
 
+answer_4 %>%
+  filter(is.na(arr_delay))
+answer_5<- "no "
